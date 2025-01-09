f8e055ded3f7eee38e4cb0beed40183770d1c23c
Guilherme <guilherme@MacBook-Pro-de-Guilherme.local>
2025-01-09 15:07:45 +0000

    implementing stacks

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,43 +6,60 @@
 /*   By: gribeiro <gribeiro@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 00:50:30 by gribeiro          #+#    #+#             */
-/*   Updated: 2025/01/09 03:32:29 by gribeiro         ###   ########.fr       */
+/*   Updated: 2025/01/09 15:05:53 by gribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static t_node	*fillfromstring(int argc, char **argv, t_node *stack_a, t_node *stack_b);
+static char		**filllist(char **argv, int argc, int freeflag);
+static int		checkerrors(char **argv, int argc);
 static t_node	*fillstack(int argc, char **argv, t_node *stack_a, t_node *stack_b);
-static int		checkerrors(t_node *stack_a);
-/*
+static void		freemem(char **list, t_node *stack_a, t_node *stack_b, int freeflag);
 
-	Verificações iniciais:
-		ERRO se for passado apenas 1 argumento
-		ERRO se forem 2 argumentos com string vazia
-	
-*/
 int		main(int argc, char **argv)
 {
 	t_node	*stack_a;
 	t_node	*stack_b;
+	char	**list;
+	int		freeflag;
 	
-	// caso não sejam atribuidos args ou atrib string vazia.
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
-	// se os valor forem dados com string
+	argv++;
+	list = argv;
 	if (argc == 2)
-		fillfromstring (argc, argv, stack_a, stack_b);
-	// se os valor forem dados com varios args
-	if (argc > 2)
-		fillstack (argc, argv, stack_a, stack_b);
-	// verificar erros
-	if (checkerrors (stack_a) != 0)
-		return (1); // libertar lista
+		list = filllist (argv, &argc, &freeflag);
+	if (checkerrors (list, argc) != 0)
+	{
+		free (list);
+		return (write (1, "Error", 5), 1);
+	}
+
+
+	freemem (list, stack_a, stack_b, freeflag);
 	return (0);
 }
 
-static t_node	*fillstack(int argc, char **argv, t_node *stack_a, t_node *stack_b)
+static char	**filllist(char **argv, int argc, int freeflag)
+{
+	char	**list;
+
+	list = ft_split (argv[1], ' ');
+	argc = 0;
+	while (list[argc])
+		argc++;
+	argc += 1;
+	freeflag = 1;
+	return (list);
+}
+
+static int	checkerrors(char **argv, int argc)
+{
+	return (0);
+}
+
+static t_node	*fillstack(int argc, char **list, t_node *stack_a, t_node *stack_b)
 {
 	stack_a = ft_calloc (argc, sizeof(t_node));
 	if (stack_a == NULL)
@@ -50,18 +67,11 @@ static t_node	*fillstack(int argc, char **argv, t_node *stack_a, t_node *stack_b
 	stack_b = ft_calloc (argc, sizeof(t_node));
 	if (stack_b == NULL)
 		return (NULL);
-	while (argc > 1)
-	{
-		
-	}
+
 	return (stack_a);
 }
-static t_node	*fillfromstring(int argc, char **argv, t_node *stack_a, t_node *stack_b)
+
+static void freemem (char **list, t_node *stack_a, t_node *stack_b, int freeflag)
 {
 	
-}
-
-static int	checkerrors(t_node *stack_a)
-{
-	return (0);
 }
